0657ff6bb9e866973bed869929553cde7f1576db
AmarAthuri <ar@ar.com>
2020-04-27 22:55:25 +0530

    ACCEPTANce git commit

--- JavaWorld.java
+++ JavaWorld.java
@@ -3,7 +3,7 @@ public class JavaWorld {
 public static void main (String[] args) {
 
 System.out.println("######################This is From JavaWorld###########################");
-System.out.println("################ GIT UNCOMMITTEd#######################");
+System.out.println("################ GIT ACCEPTANCE#######################");
 
 }
 }
